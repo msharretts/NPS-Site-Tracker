d67b15924f1f74274a00cdb098b03d59da26084e
Megan Sharretts <megsharretts@gmail.com>
2024-04-06 12:16:22 -0400

    renamed and reorganized repository

---
+++ java/src/main/java/com/techelevator/controllers/NpsSiteController.java
@@ -0,0 +1,30 @@
+package com.techelevator.controllers;
+
+import com.techelevator.dao.NpsSiteDao;
+import com.techelevator.dao.UsaStateDao;
+import com.techelevator.model.NpsSite;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class NpsSiteController {
+
+    private final NpsSiteDao npsSiteDao;
+    private final UsaStateDao usaStateDao;
+
+
+    public NpsSiteController(NpsSiteDao npsSiteDao, UsaStateDao usaStateDao) {
+        this.npsSiteDao = npsSiteDao;
+        this.usaStateDao = usaStateDao;
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(path = "/")
+    public NpsSite getSite(Principal principal) {
+        
+    }
+}
